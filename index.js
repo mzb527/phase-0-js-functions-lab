eab34b17fc559ad1235b1b2e794366e4a1aca1ab
mzb527 <mzb527@gmail.com>
2025-03-08 17:56:48 -0500

    Integrated Functions
    Integrated functions for the lab

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-
-
-
+// Function 1: calculatetax
+function calculatetax(amount) {
+    return amount * 0.1; // 10% tax
+  }
+  
+  // Function 2: convertToUpperCase
+  function convertToUpperCase(text) {
+    return text.toUpperCase();
+  }
+  
+  // Function 3: findMaximum
+  function findMaximum(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+  }
+  
+  // Function 4: isPalindrome
+  function isPalindrome(word) {
+    const reversed = word.split('').reverse().join('');
+    return word === reversed;
+  }
+  
+  // Function 5: calculateDiscountedPrice
+  function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    return originalPrice - (originalPrice * discountPercentage / 100
 
 
 // This is required for the test to function properly  
